2f9c54d1cb8d5275aeba4c268b5f2b2573202cbb
sasamir10 <sabbirahmed.samir18@gmail.com>
2026-07-27 02:47:23 +0600

    FizzBuzz problem

--- problem4.js
+++ problem4.js
@@ -1,6 +1,6 @@
 // fizzBuzz problem
 
-let number = 15;
+/* let number = 15;
 
 for (let i = 1; i <= number; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
@@ -12,4 +12,64 @@ for (let i = 1; i <= number; i++) {
     } else {
         console.log(i);
     }
+} */
+
+/* function fizzBuzz(number) {
+    let result;
+    for (let i = 1; i <= number; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result = "FizzBuzz";
+        } else if (i % 3 === 0) {
+            result = "Fizz";
+        } else if (i % 5 === 0) {
+            result = "Buzz";
+        } else {
+            result = i;
+        }
+    }
+
+    return result;
 }
+
+console.log(fizzBuzz(15)); */
+
+/* function fizzBuzz(number) {
+    const result = [];
+
+    for (let i = 1; i <= number; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i);
+        }
+    }
+
+    return result;
+}
+
+const output = fizzBuzz(15);
+
+console.log(output); */
+
+function fizzBuzz(number) {
+    let result = [];
+
+    for (let i = 1; i <= number; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i);
+        }
+    }
+    return result;
+}
+
+console.log(fizzBuzz(15));
